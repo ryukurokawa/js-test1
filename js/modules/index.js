d1e0cfb964224f6b830fe56833f6788fdcf9269b
りゅう <setsuna1228@gmail.com>
2025-10-25 00:04:59 +0900

    動作確認中

--- js/modules/index.js
+++ js/modules/index.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector('#nav');
+const cover =document .querySelector('#cover')
 const appNames =['todo','typing','slide-puzzle','memory-card','life'];
 
 appNames.forEach(appName=>{
@@ -6,8 +7,18 @@ appNames.forEach(appName=>{
   menu.classList.add('nav-menu') 
   menu.textContent = appNames.toUpperCase();
   menu.addEventListener('click',()=>{
+    cover.classList.remove('active');
+    const appEls =document.querySelectorAll('.app')
+    appEls.forEach(appEl=>{
+      appEl.classList.remove('active')
+    })
     const appEl =document.getElementById(appName)
     appEl.classList.add('active')
+    const navMenus =document.querySelectorAll('.nav-menu')
+    navMenus.forEach(navMenu=>{
+      navMenu.classList.remove('active')
+    })
+    menu.classList.add('active')
   })
   nav.appendChild(menu)
 })
