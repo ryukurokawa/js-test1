def8e9c3ec9eca7a5478261f1bcd585d01c9421b
りゅう <setsuna1228@gmail.com>
2025-10-28 23:35:32 +0900

    かくにん

--- js/modules/todo.js
+++ js/modules/todo.js
@@ -1,6 +1,9 @@
-const addForm =document.querySelector('.td-add-form')
+const addForm = document.querySelector('.td-add-form')
 const addInput = document.querySelector('.td-add-input')
+const todosUI = document.querySelector('.todos')
+const donesUI = document.querySelector('.dones')
 
+let todoData=[]
 addForm.addEventListener('submit',e => {
   //デフォルトのリロードを抑制するため
   e.preventDefault()
@@ -8,5 +11,72 @@ addForm.addEventListener('submit',e => {
      //.valueにすることによって入力値を取得することができる
     content:addInput.value.trim(),
     isDone:false
+};
+//todoObjのcontent内が空白でない場合のみ、pushするようにする
+if(todoObj.content){
+  todoData.push(todoObj);
 }
+addInput.value='';
+updateLs();
 })
+
+//ローカルストレージにデータを保存する
+function updateLs(){
+  localStorage.setItem('myTodo',JSON.stringify(todoData));
+}
+
+function getTodoData(){
+  return JSON.parse(localStorage.getItem('myTodo'))
+}
+
+function createTodoElement(todo){
+  const todoItem = document.createElement('li');
+  todoItem.classList.add('td-item')
+  const todoContent =document.createElement('p')
+  todoContent.classList.add('td-content')
+  todoContent.textContent = todo.content;
+  todoItem.appendChild(todoContent)
+
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('td-btn-container')
+  const btn = document.createElement('img')
+  btn.classList.add('td-btn');
+  const upBtn =btn.cloneNode(false)
+  upBtn.setAttribute('src','./images/todo_button/up.png')
+
+  //TODOが「未完了」か「完了済み」かで、表示されるリストやボタンを切り替える処理
+  if(!todo.isDone){
+    upBtn.classList.add('edit-btn')
+    btn.classList.add('isDone-btn')
+    btn.setAttribute('src','./images/todo_button/ok.png')
+    btnContainer.appendChild(btn)
+    btnContainer.appendChild(upBtn)
+    todoItem.appendChild(btnContainer)
+    todosUI.appendChild(todoItem)
+  }else{
+    upBtn.classList.add('undo-btn')
+    btn.classList.add('delete-btn')
+    btn.setAttribute('src','./images/todo_button/cancel.png')
+    btnContainer.appendChild(btn)
+    btnContainer.appendChild(upBtn)
+    todoItem.appendChild(btnContainer)
+    donesUI.appendChild(todoItem)
+  }
+  todoItem.addEventListener('click',e=>{
+    if(e.target.classList.contains('isDone-btn')){
+      todo.isDone = true;
+    }
+    if(e.target.classList.contains('undo-btn')){
+      todo.isDone = false;
+    }
+    if(e.target.classList.contains('edit-btn')){
+      
+    }
+    if(e.target.classList.contains('delete-btn')){
+      
+    }
+  })
+}
+
+createTodoElement({content:'remains to do',isDone:false});
+createTodoElement({content:'already done', isDone:true})
