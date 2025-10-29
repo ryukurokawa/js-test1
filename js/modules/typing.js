e1c4051aedc11e78b3008c3c99d3a777c9a44e59
りゅう <setsuna1228@gmail.com>
2025-10-29 23:49:35 +0900

    開発途中

---
+++ js/modules/typing.js
@@ -0,0 +1,28 @@
+const startPage =document.querySelector('#ty-start-page')  //document.querySelectorは一致した一つの要素を取得する関数
+const typingGame = document.querySelector('#ty-game')
+const titleTime = document.querySelector('#ty-title-time')
+const timer = document.querySelector('#ty-timer')
+const timeSelectEl = document.querySelector('.ty-time-select');
+
+let timelimit=30;
+let remaingTime;
+
+timeSelectEl.addEventListener('change', () => {
+  timelimit = parseInt(timeSelectEl.value, 10); // 数字に変換
+});
+
+window.addEventListener('keypress',e=>{
+  if(e.key === 'Enter'){
+   
+    start();
+  }
+  return;
+})
+
+function start(){
+  startPage.classList.remove('show')
+  typingGame.classList.add('show')
+  titleTime.textContent =timelimit;
+  remainigTime = timelimit;
+  timer.textContent = remainingTime;
+}
